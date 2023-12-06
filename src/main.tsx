35d8ae81aca7fe3ab69ac253e546ba06ec7de038
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2023-12-06 13:29:31 +0100

    init from https://github.com/css-hooks/css-hooks/tree/master/examples/react

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import "@fontsource/inter/latin-700.css";
+import { hooks } from "./css-hooks";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <style dangerouslySetInnerHTML={{ __html: hooks }} />
+    <App />
+  </React.StrictMode>,
+);
